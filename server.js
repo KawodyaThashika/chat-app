bfe5d7ec034784739fa2d69ac9af595cd453de6f
Kawodya Thashika <kawodyathashika@gmail.com>
2026-03-11 17:01:18 +0530

    Use environment variables for MySQL connection

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // MySQL Connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "chat_app"
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "chat_app",
+    port: process.env.DB_PORT || 3306
 });
-
 db.connect(err => {
     if (err) throw err;
     console.log("MySQL Connected");
